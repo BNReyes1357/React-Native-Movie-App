9e307080b579f54cd670f234f8e9077782cb7ae7
Ben Reyes <benstudying2468@gmail.com>
2025-10-14 19:37:54 -0500

    create styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // NOTE: Update this to include the paths to all of your component files.
+    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#030014'
+            }
+        },
+    },
+    plugins: [],
 }
-
